9aeccd2d0bef2de2d99a2b460e3777ae2ec31e53
tinyfatboy <tinyfatboy@outlook.com>
2017-09-18 16:04:09 +0800

    Complete tab component

--- Tab_Component/tab.js
+++ Tab_Component/tab.js
@@ -1,3 +1,20 @@
 /**
  * Created by 小胖子 on 2017/9/18.
  */
+window.tabs = function(element){
+    var $tabs = $(element)
+
+    let selector = 'ol[data-role="nav"]>li'
+    $tabs.on('click', selector, e=>{
+        let $li = $(e.currentTarget)
+        let index = $li.index()
+
+        $li.addClass('active').siblings().removeClass('active')
+
+    $li.closest('ol[data-role="nav"]').siblings('ol[data-role="panes"]').find('li').eq(index).addClass('active').siblings().removeClass('active')
+
+})
+
+}
+
+tabs(document.querySelector('.tabs'))
