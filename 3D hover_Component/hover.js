e53efb235fdabe525bd224385a9afd11dab2f2f9
tinyfatboy <tinyfatboy@outlook.com>
2017-09-18 18:39:26 +0800

    complete hover component

--- 3D hover_Component/hover.js
+++ 3D hover_Component/hover.js
@@ -1,3 +1,27 @@
 /**
  * Created by 小胖子 on 2017/9/18.
  */
+var container = document.querySelector('.container')
+var banner = document.querySelector('.banner')
+container.addEventListener('mousemove', function(e) {
+//  console.log(clientX, e.clientY)
+    let width = container.getBoundingClientRect().width
+    let height = container.getBoundingClientRect().height
+    let xCenter = container.offsetLeft + width / 2
+    let yCenter = container.offsetTop + height / 2
+    let xDiff = e.clientX - xCenter
+    let yDiff = e.clientY - yCenter
+
+//  let distance = Math.sqrt(xDiff*xDiff + yDiff*yDiff)
+//  let maxDistance = Math.sqrt(width*width + height*height)
+
+    let xPercent = xDiff / width * 2
+    let yPercent = yDiff / height * 2
+    console.log(xPercent, yPercent)
+
+    let xDeg = xPercent * 9
+    let yDeg = yPercent * 12
+
+    banner.style.transform = `translateZ(-10px) rotateX(${-yDeg}deg) rotateY(${xDeg}deg)`
+})
+
